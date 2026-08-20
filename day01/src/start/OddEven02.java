92446fd73bec2cdf35d4232b39000f444f1b8ba6
uteng0911 <uteng0911@gmail.com>
2026-08-20 19:56:00 +0900

    .

--- day01/src/start/OddEven02.java
+++ day01/src/start/OddEven02.java
@@ -1,10 +1,27 @@
 package start;
 
+import java.util.Scanner;
+
 public class OddEven02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요 : ");
+		int num = sc.nextInt();
+		
+		if (num>0) {
+			if (num % 2 ==0) {
+				System.out.println("짝수입니다.");
+			}else {
+					System.out.println("홀수입니다.");
+			}
+			}else {
+				System.out.println("양수가 아닙니다.");
+					
+				
+			}
+		}
 
 }
+
