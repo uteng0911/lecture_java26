66995d0404122018b0ebf69f31df01926622ec41
uteng0911 <uteng0911@gmail.com>
2026-08-20 17:53:33 +0900

    .

--- day01/src/start/Rectangle.java
+++ day01/src/start/Rectangle.java
@@ -1,13 +1,26 @@
 package start;
 
+import java.util.Scanner;
+
 public class Rectangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double a = 2.2;
-		double b = 10.0;
-System.out.println(a);
-System.out.println(b);
+Scanner sc = new Scanner(System.in);
+System.out.println("직사각형의 가로 길이 : ");
+// 가로길이 입력하기
+double a = sc.nextDouble();
+
+System.out.println("직사각형의 세로 길이 : ");
+// 세로길이 입력하기
+double b = sc.nextDouble();
+
+// 결과 출력
+double area = a * b;
+
+System.out.println("");
+System.out.println("직사각형의 넓이 = " + area);
+
 
 
 	}
