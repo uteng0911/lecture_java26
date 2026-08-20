b0628b89e4e57c7ad30850ff318124e6f7f6ac30
uteng0911 <uteng0911@gmail.com>
2026-08-20 20:44:54 +0900

    .

--- day01/src/start/While02.java
+++ day01/src/start/While02.java
@@ -1,10 +1,32 @@
 package start;
 
+import java.util.Scanner;
+
 public class While02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int go = 1;
+				while (go == 1) {
+				System.out.println("숫자를 입력하세요 : ");
+				int num = sc.nextInt();
+				
+				if (num % 2 == 0){
+				System.out.println(">>짝수입니다.");
+	} else 
+		System.out.println(">>홀수입니다.");
+				{
+		
+		System.out.println("계속 하시겠습니까? (0-멈춤/1-계속) : ");
+	    go = sc.nextInt();
+	    System.out.println();
+		
+		
 	}
-
+				}
+	}
 }
+	
+
+
