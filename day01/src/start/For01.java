bac8b36ca7a33aa360dec9f280e1781e7052740d
uteng0911 <uteng0911@gmail.com>
2026-08-20 21:13:56 +0900

    .

--- day01/src/start/For01.java
+++ day01/src/start/For01.java
@@ -1,9 +1,24 @@
 package start;
 
+import java.util.Scanner;
+
 public class For01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요 : ");
+		int num = sc.nextInt();
+		for (int i = 1; i <= num; i++) {
+			sum += i;
+		}
+		
+		
+		System.out.println("1부터" + num + "까지의 합은" + sum + "입니다.");
+		
+		
+		
 
 	}
 
