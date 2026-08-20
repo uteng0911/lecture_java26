d287f8e70daf94c12ce02407c66bfb1aab6cbdf9
uteng0911 <uteng0911@gmail.com>
2026-08-20 17:31:27 +0900

    .

--- day01/src/start/Swap.java
+++ day01/src/start/Swap.java
@@ -5,20 +5,25 @@ import java.util.Scanner;
 public class Swap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 숫자 2개를 입력받고
 Scanner sc = new Scanner(System.in);
-System.out.println("변수 a에 넣을 숫자 입력 : 10");
-int a = 10;
-System.out.println("변수 b에 넣을 숫자 입력 : 25");
-int b = 25;
 
+System.out.println("변수 a에 넣을 숫자 입력 : ");
+int a = sc.nextInt();
 
+System.out.println("변수 b에 넣을 숫자 입력 : ");
+int b = sc.nextInt();
 
+// 숫자 2개를 swap
+int temp;
+temp = a;
+a = b;
+b = temp;
 
-
-
-
-;
+// swap 한 결과 출력
+System.out.println("Swapping 결과 >");
+System.out.println("변수 a의 값 = " + a);
+System.out.println("변수 b의 값 = " + b);
 
 	}
 
