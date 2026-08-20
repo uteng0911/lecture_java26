442460c213bfd3cfbf0b43b837e8c90f06116b38
uteng0911 <uteng0911@gmail.com>
2026-08-20 20:17:06 +0900

    .

--- day01/src/start/While01.java
+++ day01/src/start/While01.java
@@ -1,9 +1,25 @@
 package start;
 
+import java.util.Scanner;
+
 public class While01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요 : ");
+		int num = sc.nextInt();
+		
+		while (num != 0) {
+			sum += num;
+		
+		System.out.println("숫자를 입력하세요 : ");
+		num = sc.nextInt();
+		}
+		
+		System.out.println("지금까지 입력한 숫자의 합은" + sum + "입니다.");
 
 	}
 
